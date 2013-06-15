db491d73d4eddd6b04b9930004378969457855bb
Thijs Jonkman <t.jonkman@gmail.com>
2013-06-16 00:55:07 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='diazotheme.bootstrap',
+      version=version,
+      description="",
+      long_description=open("README.md").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='TH-code',
+      author_email='t.jonkman@gmail.com',
+      url='https://github.com/TH-code',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['diazotheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'diazo.framework.bootstrap',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
